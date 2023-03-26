d52dc4bca7bf188250f8d2c2e40313df1f0f50de
Matias Diez-Canseco <matiasdiezcanseco@gmail.com>
2023-03-25 22:45:10 -0500

    Fixed 0 pos bug

--- src/generateImage.ts
+++ src/generateImage.ts
@@ -1,6 +1,6 @@
 import { imagesUrls } from '../utils/images'
 
 export const generateImage = (pos?: number) => {
-  if (pos && imagesUrls[pos]) return process.env.BUCKET_URL + imagesUrls[pos]
+  if (typeof pos === 'number' && imagesUrls[pos]) return process.env.BUCKET_URL + imagesUrls[pos]
   return process.env.BUCKET_URL + imagesUrls[Math.floor(Math.random() * imagesUrls.length)]
 }
--- src/generatePhrase.ts
+++ src/generatePhrase.ts
@@ -1,6 +1,6 @@
 import { phrases } from '../utils/phrases'
 
 export const generatePhrase = (pos?: number) => {
-  if (pos && phrases[pos]) return phrases[pos]
+  if (typeof pos === 'number' && phrases[pos]) return phrases[pos]
   return phrases[Math.floor(Math.random() * phrases.length)]
 }
